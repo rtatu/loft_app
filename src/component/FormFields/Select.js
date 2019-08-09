ffd55bcf285eab68b79642278a72fa919489eae7
Rohit Tatu <rtatu@Rohits-MacBook-Air.local>
2019-08-09 13:21:53 +0530

    asd

--- src/component/FormFields/Select.js
+++ src/component/FormFields/Select.js
@@ -3,21 +3,130 @@ import down from '../../../static/icon/down.svg'
 
 const values = ['ACTIVE', 'INACTIVE']
 
+let data = [
+	{"name": "Rajah"},
+	{"name": "Burke"},
+	{"name": "Wesley"},
+	{"name": "Alan"},
+	{"name": "Cooper"},
+	{"name": "Drake"},
+	{"name": "Roth"},
+	{"name": "Fitzgerald"},
+	{"name": "Ashton"},
+	{"name": "Tobias"},
+	{"name": "Linus"},
+	{"name": "Garrison"},
+	{"name": "Nasim"},
+	{"name": "Keith"},
+	{"name": "Sawyer"},
+	{"name": "Lamar"},
+	{"name": "Eaton"},
+	{"name": "Garth"},
+	{"name": "Francis"},
+	{"name": "Kyle"},
+	{"name": "Steven"},
+	{"name": "Darius"},
+	{"name": "Rafael"},
+	{"name": "Abel"},
+	{"name": "Kane"},
+	{"name": "Damian"},
+	{"name": "Cairo"},
+	{"name": "Prescott"},
+	{"name": "Rooney"},
+	{"name": "Henry"},
+	{"name": "Jeremy"},
+	{"name": "Chancellor"},
+	{"name": "Dexter"},
+	{"name": "Callum"},
+	{"name": "Hilel"},
+	{"name": "Macon"},
+	{"name": "Alec"},
+	{"name": "Leo"},
+	{"name": "Stuart"},
+	{"name": "Beck"},
+	{"name": "Derek"},
+	{"name": "Ciaran"},
+	{"name": "Jasper"},
+	{"name": "Aaron"},
+	{"name": "Hector"},
+	{"name": "Jelani"},
+	{"name": "Leo"},
+	{"name": "Trevor"},
+	{"name": "Ciaran"},
+	{"name": "Grady"},
+	{"name": "Christopher"},
+	{"name": "Hakeem"},
+	{"name": "Jasper"},
+	{"name": "Reed"},
+	{"name": "Clarke"},
+	{"name": "Oleg"},
+	{"name": "Thane"},
+	{"name": "Ignatius"},
+	{"name": "Coby"},
+	{"name": "Micah"},
+	{"name": "Herrod"},
+	{"name": "Marvin"},
+	{"name": "Tyrone"},
+	{"name": "Ezekiel"},
+	{"name": "Fritz"},
+	{"name": "Blaze"},
+	{"name": "Clinton"},
+	{"name": "Thomas"},
+	{"name": "Brendan"},
+	{"name": "Axel"},
+	{"name": "Chancellor"},
+	{"name": "Dane"},
+	{"name": "Garrison"},
+	{"name": "Timon"},
+	{"name": "Ryder"},
+	{"name": "Odysseus"},
+	{"name": "Raja"},
+	{"name": "Jameson"},
+	{"name": "Addison"},
+	{"name": "Noble"},
+	{"name": "Maxwell"},
+	{"name": "Caleb"},
+	{"name": "Warren"},
+	{"name": "Colin"},
+	{"name": "Sean"},
+	{"name": "Zeus"},
+	{"name": "Drew"},
+	{"name": "Owen"},
+	{"name": "Noble"},
+	{"name": "Alec"},
+	{"name": "Nasim"},
+	{"name": "Abraham"},
+	{"name": "Judah"},
+	{"name": "Hedley"},
+	{"name": "Reed"},
+	{"name": "David"},
+	{"name": "Geoffrey"},
+	{"name": "Demetrius"},
+	{"name": "George"},
+	{"name": "Benedict"}
+];
+
+data = data.map(item => item.name)
+
 class Select extends React.Component{
     // constructor
 
     constructor(props) {
-        console.log(props)
         super(props)
 
         // create refs
 
         this.state = {
-            show : false
+            show : false,
+            suggestions : data,
+            value : this.props.value || "",
+            select : -1
         }
 
         this.input = React.createRef()
         this.list = React.createRef()
+        this.dropDown = React.createRef()
+        this.selected = React.createRef()
     }
 
     componentDidMount(){
@@ -29,13 +138,19 @@ class Select extends React.Component{
     }
 
     handleClick = (e) => {
+
         if(e.target == this.input.current){
             e.preventDefault()
             this.input.current.focus()
             return
         }
 
-        this.setState({show : false})
+        if (e.target == this.dropDown.current) {
+            e.preventDefault()
+            return
+        }
+
+        this.input.current.blur()
 
     }
 
@@ -44,28 +159,57 @@ class Select extends React.Component{
         e.preventDefault()
 
         if(!this.state.show){
-                this.input.current.focus()
+            this.input.current.focus()
         }else{
-                this.input.current.blur()    
+            this.input.current.blur()    
         }
 
     }
 
 
     onInputFocus = (e) => {
-        this.setState({show : true}, () => console.log(this.state))
+        this.setState({show : true})
     }
 
     onInputBlur = (e) => {
-        this.setState({show : false})
+        this.setState({show : false, select : -1})
+    }
+
+    handleSuggestions = (e) => {
+
+        e.preventDefault()
+
+        this.setState({value : e.target.innerText})
+
+    }
+
+    handleChange = (e) => {
+        this.setState({value : e.target.value})
+    }
+
+    onKeyDown = (e) => {
+
+        if(e.keyCode == 40 && this.state.select + 1 < this.state.suggestions.length) {
+
+                this.setState({select : this.state.select + 1}, () => {
+                    this.selected.current.scrollIntoView({block : 'nearest'});
+                })
+                
+            
+        } else if (e.keyCode == 38 && this.state.select - 1 >= 0) {
+            this.setState({select : this.state.select - 1}, () => {
+                this.selected.current.scrollIntoView({block : 'nearest'});
+            })
+        } else if (e.keyCode == 13) {
+            if(this.selected.current !== null && this.selected.current !== undefined) {
+                this.setState({value : this.selected.current.innerText}, () => this.input.current.blur())
+            }
+        }
+
     }
 
 
-
     render(){
-        if(this.state.show){
-            console.log('1')
-        }
         return(
             <div className="field">
                 <label>
@@ -76,19 +220,28 @@ class Select extends React.Component{
                     ref={this.input}
                     onBlur={this.onInputBlur}
                     onFocus={this.onInputFocus}
+                    value = {this.state.value}
+                    onChange = {this.handleChange}
+                    onKeyDown = {this.onKeyDown}
                 />
                 <img 
                     src={down} 
                     className="select-down" 
-                    onClick={this.onDownClick}
+                    onMouseDown={this.onDownClick}
+                    ref={this.dropDown}
                 />
                 {
                     (this.state.show)
                     ?
                     <ul className="select" ref={this.list}>
                         {
-                            values.map( (value, index) => 
-                                <li key={index}>
+                            this.state.suggestions.map( (value, index) => 
+                                <li 
+                                    key={index}
+                                    ref={(index == this.state.select) ? this.selected : null} 
+                                    onMouseDown={this.handleSuggestions}
+                                    className={(index == this.state.select) ? "selected" : null}
+                                >
                                     {value}
                                 </li>
                             )
